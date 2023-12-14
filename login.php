1865872954b160fdcb0eaa0a743038c1bac72e3a
A.Lejeune <acelyalejeune@gmail.com>
2023-12-14 18:16:32 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,21 +1,26 @@
 <?php
-    require_once('settings.php');
+require_once('settings.php');
 
-    /**
-     * ICI VOUS ECRIVEZ LE CODE PHP QUI GERE LA LOGIQUE ET LES DONNEES DE l'APPLICATION
-     */
+/**
+ * ICI VOUS ECRIVEZ LE CODE PHP QUI GERE LA LOGIQUE ET LES DONNEES DE l'APPLICATION
+ */
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <?php displayHeadSection('S\'identifier'); ?>
 </head>
+
 <body>
+
     <div class="container">
-            <div id="header-logo">
-                <h1><?php echo APP_NAME; ?></h1>
-            </div>
+        <div id="header-logo">
+            <h1><a href="index.php" title="Aller sur la page d'accueil du Blog"> <?php echo APP_NAME; ?></a></h1>
+        </div>
+    </div>
+        <div class="container">
             <div id="main-menu">
                 <?php displayNavigation(); ?>
             </div>
@@ -31,17 +36,12 @@
                           C'est ici même dans login.php que nous traiterons l'identification
                         - L'attribut "method" devra contenir "post"                    
                 -->
-                                
-            </div>  
+
+            </div>
             <footer>
-                 <!-- 
-                    Ouvrez une balise php pour lancer la fonction d'affichage 
-                    du footer. Fonction que vous allez écrire dans fct-ui.php
-                    Affichez le nom de l'app sa version sa date de mise à jour
-                    et d'autres choses si vous le souhaitez 
-                -->
-            </footer>     
+                <?php echo displayFooterSection(); ?>
+            </footer>
         </div>
-    </div>    
 </body>
+
 </html>
