6d7db717f224fd747fbd48ba6d5152f6e3ce3acc
A.Lejeune <acelyalejeune@gmail.com>
2023-12-14 16:17:09 +0100

    Update index.php

--- index.php
+++ index.php
@@ -21,29 +21,10 @@
                 <?php displayNavigation(); ?>
             </div>
             <div id="content">
-                <!-- 
-                    Ouvrez une balise php pour lancer la fonction d'affichage 
-                    des articles publiés. Fonction que vous allez écrire dans fct-ui.php 
-                -->
-                <!-- Exemple en HTML, vous, vous devez créer une fonction qui va afficher
-                     les données de la DB. SUPPRIMEZ L'EXEMPLE.
-                -->
-                <p><a href="article.php?id=xx" class="titre-article">Titre du premier article</a></p>
-                <p><a href="article.php?id=xx" class="titre-article">Titre du second article</a></p>
-                <p><a href="article.php?id=xx" class="titre-article">Titre du troisième article</a></p>
-                
+                <?php echo displayArticlesPublies(); ?>
             </div>  
             <footer>                
-                <!-- 
-                    Ouvrez une balise php pour lancer la fonction d'affichage 
-                    du footer. Fonction que vous allez écrire dans fct-ui.php
-                    Affichez le nom de l'app sa version sa date de mise à jour
-                    et d'autres choses si vous le souhaitez 
-                -->
-                <!-- Exemple en HTML du footer, vous, vous devez créer une fonction qui va afficher
-                     les données de la DB. SUPPRIMEZ L'EXEMPLE.
-                -->
-                <p>Superblog - v0.0.1 - 13-12-2023 16:45 by Vous<p>
+                <?php echo displayFooterSection(); ?>
             </footer>     
         </div>
     </div>    
