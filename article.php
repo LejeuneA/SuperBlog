e9af117fbbef8697771f80d247ced76df50f0573
A.Lejeune <acelyalejeune@gmail.com>
2023-12-15 16:38:30 +0100

    Update article.php

--- article.php
+++ article.php
@@ -1,44 +1,72 @@
 <?php
-    require_once('settings.php');
+require_once('settings.php');
 
-    /**
-     * ICI VOUS ECRIVEZ LE CODE PHP QUI GERE LA LOGIQUE ET LES DONNEES DE l'APPLICATION
-     */
+/**
+ * ICI VOUS ECRIVEZ LE CODE PHP QUI GERE LA LOGIQUE ET LES DONNEES DE l'APPLICATION
+ */
 
+if (isset($_GET['id'])) {
+    // Get the article ID from the URL
+    $articleId = $_GET['id'];
+
+    // Retrieve the article based on its ID
+    $article = getArticleByIDDB($conn, $articleId);
+
+    // Check if the article exists
+    if ($article) {
+        // Set the page title to the article title
+        $pageTitle = $article['title'];
+    } else {
+        // If the article does not exist, set a default title
+        $pageTitle = 'Article Not Found';
+    }
+} else {
+    // If 'id' parameter is not set, set a default title
+    $pageTitle = 'Article Not Found';
+}
 ?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <?php displayHeadSection('***Titre de l\'article***'); ?>
+    <?php displayHeadSection($pageTitle); ?>
 </head>
+
 <body>
     <div class="container">
-            <div id="header-logo">
-                <h1><?php echo APP_NAME; ?></h1>
-            </div>
-            <div id="main-menu">
-                <?php displayNavigation(); ?>
-            </div>
-            <h1>Afficher l'article passé en paramètre</h1>
-            <div id="message">
-                <!-- Ici nous affichons les messages éventuels (CODE PHP) -->
-            </div>
-            <div id="content">
-                <!-- 
-                      Vous devez créer une fonction d'affichage pour afficher l'article:
-                      Son titre et son contenu
-                -->
-                                
-            </div>  
-            <footer>
-                 <!-- 
-                    Ouvrez une balise php pour lancer la fonction d'affichage 
-                    du footer. Fonction que vous allez écrire dans fct-ui.php
-                    Affichez le nom de l'app sa version sa date de mise à jour
-                    et d'autres choses si vous le souhaitez 
-                -->
-            </footer>     
+        <div id="header-logo">
+            <h1><a href="index.php" title="Aller sur la page d'accueil du Blog"> <?php echo APP_NAME; ?></a></h1>
         </div>
-    </div>    
+        <div id="main-menu">
+            <?php displayNavigation(); ?>
+        </div>
+
+        <div id="message">
+            <!-- Ici nous affichons les messages éventuels (CODE PHP) -->
+        </div>
+        <div id="content">
+
+            <?php
+            // Check if the article exists
+            if ($article) {
+                // Display the article title and content
+                echo "<h2>{$article['title']}</h2>";
+                echo "<p>{$article['content']}</p>";
+            } else {
+                // Display a message if the article does not exist
+                echo "<p>Article not found.</p>";
+            }
+            ?>
+
+        </div>
+        <footer>
+            <?php echo displayFooterSection(); ?>
+        </footer>
+    </div>
+    </div>
 </body>
+
 </html>
