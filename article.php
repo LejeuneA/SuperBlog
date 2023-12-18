11265727ac3d6473699e68855be8704e753e8ba7
Acelya Lejeune <145855362+LejeuneA@users.noreply.github.com>
2023-12-18 21:08:29 +0100

    Update article.php

--- article.php
+++ article.php
@@ -37,32 +37,31 @@ if (isset($_GET['id'])) {
 
 <body>
     <div class="container">
-        <div id="header-logo">
-            <h1><a href="index.php" title="Aller sur la page d'accueil du Blog"> <?php echo APP_NAME; ?></a></h1>
-        </div>
-        <div id="main-menu">
-            <?php displayNavigation('Articles'); ?>
-        </div>
+        <?php echo displayHeaderLogo(); ?>
+    </div>
+    <div id="main-menu">
+        <?php displayNavigation('Articles'); ?>
+    </div>
 
-        <div id="message">
-            <?php
-            // Check if the article exists
-            if ($article) {
-                // Display the article title and content
-                echo "<h2>{$article['title']}</h2>";
-                echo "<p>{$article['content']}</p>";
-            } else {
-                // Display a message if the article does not exist
-                echo "<p>Il n'y a pas d'article à afficher.</p>";
-            }
-            ?>
-        </div>
-        <div id="content">
-            <?php echo getArticleByIDDB($conn, $id); ?>
-        </div>
-        <footer>
-            <?php echo displayFooterSection(); ?>
-        </footer>
+    <div id="message">
+        <?php
+        // Check if the article exists
+        if ($article) {
+            // Display the article title and content
+            echo "<h2>{$article['title']}</h2>";
+            echo "<p>{$article['content']}</p>";
+        } else {
+            // Display a message if the article does not exist
+            echo "<p>Il n'y a pas d'article à afficher.</p>";
+        }
+        ?>
+    </div>
+    <div id="content">
+        <?php echo getArticleByIDDB($conn, $id); ?>
+    </div>
+    <footer>
+        <?php echo displayFooterSection(); ?>
+    </footer>
     </div>
     </div>
 </body>
