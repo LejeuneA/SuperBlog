e2a8ce0246b02fc1d7513c6181034ee133d54fb4
Acelya Lejeune <145855362+LejeuneA@users.noreply.github.com>
2023-12-18 18:17:37 +0100

    Update fct-ui.php

--- app/functions/fct-ui.php
+++ app/functions/fct-ui.php
@@ -1,4 +1,52 @@
 <?php
+
+/* ********************************************************************** */
+/* *                          TOOLS FUNCTIONS                           * */
+/* *                          ---------------                           * */
+/* *        FONCTIONS D'AFFICHAGE DE L'INTERFACE UTILISATEUR            * */
+/* ********************************************************************** */
+
+/**
+* Retourne le code html des boutons radios indiquant 
+* le status de publication de l'article
+* 
+* @param boolean     $published
+* @return string
+*/
+function displayFormRadioBtnArticlePublished($published, $typeForm = 'ADD')
+{
+   
+    $html = '';
+
+    // Si c'est le formulaire d'ajout d'article
+    if($typeForm == 'ADD'){
+        $html .= '
+        <div class="form-check form-switch">
+            <input class="form-check-input" type="checkbox" value="1" id="published_article" name="published_article">          
+        </div>
+    ';
+    // Si c'est le formulaire de modification d'article
+    }elseif($typeForm == 'EDIT'){
+
+        if($published){        
+            $html .= '
+            <div class="form-check form-switch">
+                <input class="form-check-input" value="1" type="checkbox" id="published_article" name="published_article" checked>          
+            </div>
+            ';
+        }else{
+            $html .= '
+            <div class="form-check form-switch">
+                <input class="form-check-input" value="1" type="checkbox" id="published_article" name="published_article">        
+            </div>
+            ';
+        }
+    }
+
+    echo $html; 
+}
+
+
 // Fonctions qui sont liées à l'interface utilisateur
 
 
@@ -66,7 +114,18 @@ function displayArticlesPublies($res) {
 }
 
 
-
+/**
+ * Retour d'un message au format HTML
+ * 
+ * @param string $message 
+ * @param string $type 
+ * @return string 
+ */
+function getMessage($message, $type = 'success')
+{
+    $html = '<div class="msg-'.$type.'">'.$message.'</div>';
+    return $html;
+}
 
 
 /**
