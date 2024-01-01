1feef01e5c18a07c2460aca4dc643eb4126f0746
A.Lejeune <acelyalejeune@gmail.com>
2024-01-01 12:51:47 +0100

    Update add.php

--- add.php
+++ add.php
@@ -1,41 +1,62 @@
 <?php
-require_once('settings.php');
+    require_once('settings.php');
 
-/**
- * ICI VOUS ECRIVEZ LE CODE PHP QUI GERE LA LOGIQUE ET LES DONNEES DE l'APPLICATION
- */
+    // Redirection vers la page de login si l'utilisateur n'est pas connecté
+    if (!$_SESSION['IDENTIFY']) {
+        header('Location: login.php');
+    }
+    
+    $msg = null;
+    $tinyMCE = true;
+    $execute = false;
+    
+    // Vérification de la connexion à la DB
+    if(!is_object($conn)){
+        $msg = getMessage($conn, 'error');
+    }
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <?php displayHeadSection('Ajouter un article'); ?>
 </head>
-
 <body>
     <div class="container">
-        <div class="container">
-            <?php echo displayHeaderLogo(); ?>
+        <div id="header-logo">
+            <h1><?php echo APP_NAME; ?></h1>
         </div>
         <div id="main-menu">
             <?php displayNavigation(); ?>
         </div>
-        <h1>Ajouter un article<h1>
-                <div id="content">
-                    <!-- 
-                    Créez ici un formulaire HTML pour ajouter un nouvel article
-                    * Astuces :
-                        - L'attribut "action" de votre balise form devra contenir "manager.php"
-                          C'est dans le fichier manager.php que l'on va traiter les donées du formulaire
-                        - L'attribut "method" devra contenir "post"                    
-                -->
+        <h2 class="title">Ajouter un article<h2>
+        <div id="message">               
+            <?php if(isset($msg)) echo $msg; ?>
+        </div>
+        <div id="content-add">
+
+            <form action="manager.php" method="post">                    
+                <div class="form-ctrl">
+                    <label for="title" class="form-ctrl">Titre</label>
+                    <input type="text" class="form-ctrl" id="title" name="title" value="" required>
                 </div>
-
-    </div>
-    <footer>
-        <?php echo displayFooterSection(); ?>
-    </footer>
+                <div class="form-ctrl">                                          
+                    <label for="published_article" class="form-ctrl">Status de l'article <small>(publication)</small></label> 
+                    <?php displayFormRadioBtnArticlePublished(false, 'ADD'); ?>                  
+                </div>   
+                <div class="form-ctrl">
+                    <label for="content" class="form-ctrl">Contenu</label>
+                    <textarea class="" id="content" name="content" rows="8"></textarea>
+                </div>
+                <input type="hidden" id="form" name="form" value="add">
+                <button type="submit" class="btn-classic">Ajouter</button>
+            </form>
+                            
+        </div>  
+        <footer>
+            <?php displayFooter(); ?>
+        </footer>             
+    </div>  
+    <?php displayJSSection($tinyMCE); ?>    
 </body>
-
 </html>
