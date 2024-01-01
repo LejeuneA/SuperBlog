499f5341fc5f36d39b003bd44c9e3b7a9d5734c9
A.Lejeune <acelyalejeune@gmail.com>
2024-01-01 12:52:55 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,40 +1,52 @@
 <?php
-require_once('settings.php');
+    require_once('settings.php');
 
+    // Déclaration et initalisation des variables
+    $msg = null;
+    $result = null;
+    $execute = false;
 
-$res = getAllArticlesDB($conn, 1);
+    if(!is_object($conn)){       
+        $msg = getMessage($conn, 'error');
+    }else{
+        
+        // Va cherche en DB les articles publiés
+        $result = getAllArticlesDB($conn, 1);
 
+        //DEBUG// disp_ar($result);
 
-// DEBUG // Affichage brut des articles reçu de la DB // 
-// disp_ar($res, 'ARTICLES');
+        // On vérifie le retour de la fonction  getAllArticlesDB(), elle doit nous retourner un tableau 
+        // Si c'est un tableau, on continue donc on initialise $execute = true, sinon on affiche le message d'erreur retourné par la fonction     
+        (isset($result) && is_array($result))? $execute = true : $msg = getMessage($result, 'error');            
+    }
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <?php displayHeadSection('Accueil'); ?>
 </head>
-
 <body>
     <div class="container">
-        <?php echo displayHeaderLogo(); ?>
-    </div>
-    <div id="main-menu">
-        <?php displayNavigation(); ?>
-    </div>
-
-    <div class="container">
-        <div id="content">
-            <!-- Display each active article -->
-            <?php displayArticlesPublies($res); ?>
+        <div id="header-logo">
+            <h1><a href="index.php"><?php echo APP_NAME; ?></a></h1>
         </div>
-    </div>
-
-    <footer>
-        <?php echo displayFooterSection(); ?>
-    </footer>
-
+        <div id="main-menu">
+            <?php displayNavigation(); ?>
+        </div>
+        <div id="message">               
+            <?php if(isset($msg)) echo $msg; ?>
+        </div>
+        <div id="content">
+            <?php               
+                // Peut-on exécuter l'affichage des articles
+                if($execute)
+                    displayArticles($result);
+            ?>           
+        </div>  
+        <footer>                
+            <?php displayFooter(); ?>
+        </footer>             
+    </div>    
 </body>
-
 </html>
