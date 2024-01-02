b8241b4080a254ee060507c1c9d360a26e795ff0
A.Lejeune <acelyalejeune@gmail.com>
2024-01-02 15:02:50 +0100

    Update edit.php

--- edit.php
+++ edit.php
@@ -1,7 +1,7 @@
 <?php
     require_once('settings.php');
 
-    // Redirection vers la page de login si l'utilisateur n'est pas connecté
+
     if (!$_SESSION['IDENTIFY']) {
         header('Location: login.php');
     }
@@ -14,39 +14,39 @@
     if(!is_object($conn)){
         $msg = getMessage($conn, 'error');
     } else {
-        // Vérifie si un ID est présent dans l'URL
+
         if (isset($_GET['id'])) {
-            // Récupère l'ID depuis l'URL
+
             $articleId = $_GET['id'];
 
-            // Récupère les informations de l'article depuis la base de données
+
             $article = getArticleByIDDB($conn, $articleId);
 
-            // Vérifie si le formulaire de mise à jour a été soumis
+
             if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['form']) && $_POST['form'] === 'update') {
-                // Récupère les données du formulaire
+
                 $updateData = [
                     'id' => $articleId,
                     'title' => $_POST['title'],
                     'content' => $_POST['content'],
-                    'published_article' => isset($_POST['published_article']) ? 1 : 0, // Si la case est cochée, la valeur est 1, sinon 0
+                    'published_article' => isset($_POST['published_article']) ? 1 : 0, 
                 ];
 
-                // Effectue la mise à jour de l'article dans la base de données
+
                 $updateResult = updateArticleDB($conn, $updateData);
 
                 if ($updateResult === true) {
-                    // Mise à jour réussie
+
                     $msg = getMessage('L\'article a été modifié avec succès.', 'success');
-                    // Rafraîchit les données de l'article après la mise à jour
+
                     $article = getArticleByIDDB($conn, $articleId);
                 } else {
-                    // Erreur lors de la mise à jour
+
                     $msg = getMessage('Erreur lors de la modification de l\'article. Veuillez réessayer.', 'error');
                 }
             }
         } else {
-            // Si aucun ID n'est présent dans l'URL, redirige vers la page de gestion
+
             header('Location: manager.php');
         }
     }
@@ -67,7 +67,7 @@
             <?php displayNavigation(); ?>
         </div>
         <div id="content-edit">
-            <?php echo $msg; // Affiche le message de succès ou d'erreur ?>
+            <?php echo $msg; ?>
 
             <form action="edit.php?id=<?php echo $article['id']; ?>" method="post">     
                 <input type="hidden" name="id" value="<?php echo $article['id']; ?>">               
