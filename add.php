08f5936ffa47d3c8f703d0f0f52026df1f1e42be
A.Lejeune <acelyalejeune@gmail.com>
2024-01-02 15:02:24 +0100

    Update add.php

--- add.php
+++ add.php
@@ -1,7 +1,6 @@
 <?php
     require_once('settings.php');
 
-    // Redirection vers la page de login si l'utilisateur n'est pas connecté
     if (!$_SESSION['IDENTIFY']) {
         header('Location: login.php');
     }
@@ -10,27 +9,27 @@
     $tinyMCE = true;
     $execute = false;
     
-    // Vérification de la connexion à la DB
+
     if(!is_object($conn)){
         $msg = getMessage($conn, 'error');
     } else {
-        // Vérifie si le formulaire d'ajout a été soumis
+
         if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['form']) && $_POST['form'] === 'add') {
-            // Récupère les données du formulaire
+
             $addData = [
                 'title' => $_POST['title'],
                 'content' => $_POST['content'],
-                'published_article' => isset($_POST['published_article']) ? 1 : 0, // Si la case est cochée, la valeur est 1, sinon 0
+                'published_article' => isset($_POST['published_article']) ? 1 : 0, 
             ];
 
-            // Effectue l'ajout de l'article dans la base de données
+
             $addResult = addArticleDB($conn, $addData);
 
             if ($addResult === true) {
-                // Ajout réussi
+
                 $msg = getMessage('L\'article a été ajouté avec succès.', 'success');
             } else {
-                // Erreur lors de l'ajout
+
                 $msg = getMessage('Erreur lors de l\'ajout de l\'article. Veuillez réessayer.', 'error');
             }
         }
@@ -52,11 +51,11 @@
         </div>
         <h2 class="title">Ajouter un article<h2>
         <div id="message">               
-            <?php echo $msg; // Affiche le message de succès ou d'erreur ?>
+            <?php echo $msg; ?>
         </div>
         <div id="content-add">
 
-            <form action="add.php" method="post"> <!-- Correction: action should point to add.php -->                   
+            <form action="add.php" method="post">          
                 <div class="form-ctrl">
                     <label for="title" class="form-ctrl">Titre</label>
                     <input type="text" class="form-ctrl" id="title" name="title" value="" required>
