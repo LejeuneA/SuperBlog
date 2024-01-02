5751086d6e98d73c107fbfe27268866a4cb7fc3d
A.Lejeune <acelyalejeune@gmail.com>
2024-01-02 13:35:13 +0100

    Update add.php

--- add.php
+++ add.php
@@ -13,9 +13,30 @@
     // Vérification de la connexion à la DB
     if(!is_object($conn)){
         $msg = getMessage($conn, 'error');
-    }
+    } else {
+        // Vérifie si le formulaire d'ajout a été soumis
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['form']) && $_POST['form'] === 'add') {
+            // Récupère les données du formulaire
+            $addData = [
+                'title' => $_POST['title'],
+                'content' => $_POST['content'],
+                'published_article' => isset($_POST['published_article']) ? 1 : 0, // Si la case est cochée, la valeur est 1, sinon 0
+            ];
 
+            // Effectue l'ajout de l'article dans la base de données
+            $addResult = addArticleDB($conn, $addData);
+
+            if ($addResult === true) {
+                // Ajout réussi
+                $msg = getMessage('L\'article a été ajouté avec succès.', 'success');
+            } else {
+                // Erreur lors de l'ajout
+                $msg = getMessage('Erreur lors de l\'ajout de l\'article. Veuillez réessayer.', 'error');
+            }
+        }
+    }
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -24,18 +45,18 @@
 <body>
     <div class="container">
         <div id="header-logo">
-        <h1><a href="index.php"><?php echo APP_NAME; ?></a></h1>
+            <h1><a href="index.php"><?php echo APP_NAME; ?></a></h1>
         </div>
         <div id="main-menu">
             <?php displayNavigation(); ?>
         </div>
         <h2 class="title">Ajouter un article<h2>
         <div id="message">               
-            <?php if(isset($msg)) echo $msg; ?>
+            <?php echo $msg; // Affiche le message de succès ou d'erreur ?>
         </div>
         <div id="content-add">
 
-            <form action="manager.php" method="post">                    
+            <form action="add.php" method="post"> <!-- Correction: action should point to add.php -->                   
                 <div class="form-ctrl">
                     <label for="title" class="form-ctrl">Titre</label>
                     <input type="text" class="form-ctrl" id="title" name="title" value="" required>
