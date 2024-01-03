0d4da17c66f03905edcaff0360ec10b4092493d8
A.Lejeune <acelyalejeune@gmail.com>
2024-01-03 10:11:07 +0100

    Update logoff.php

--- logoff.php
+++ logoff.php
@@ -1,13 +1,15 @@
 <?php
     require_once('settings.php');
     
+    // Script de déconnexion
     unset($_SESSION);
     setcookie(session_name(), '', time()-3600);
     session_destroy();
 
+    // Configuration de la session / du cookie de session
     $name = session_name(str_replace(' ', '', APP_NAME).'_session');
     $domain = $_SERVER['HTTP_HOST'];
-    $time = time() + 3600; 
+    $time = time() + 3600; // 3600 sec = 1 heure
 
     setcookie($name, APP_NAME, [
         'expires' => $time,
@@ -18,9 +20,11 @@
         'samesite' => 'strict',
     ]);
 
-
+    // Lancement de la session
     session_start();
 
+    // Initialisation de la variable $_SESSION['IDENTIFY'] à false (pas d'utilisateur connecté)
     $_SESSION['IDENTIFY'] = false;
 
+    // Redirection vers la page d'accueil
     header('Location: index.php');
