8c4605123f9d1a8e35f25e0c82c0646cefcc7cc8
A.Lejeune <acelyalejeune@gmail.com>
2024-01-03 10:11:02 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,6 +1,7 @@
 <?php
     require_once('settings.php');
 
+    // Redirection vers la page de gestion si l'utilisateur est connecté
     if ($_SESSION['IDENTIFY']) {
         header('Location: manager.php');
     }
@@ -9,30 +10,41 @@
     $connexionSuccessfull = null;
     $msg = null;
 
+    // On vérifie l'objet de connexion $conn
     if(!is_object($conn)){
         $msg = getMessage($conn, 'error');
     }else{
 
+        // Vérifie si on reçoit le formumaire d'identification
         if(isset($_POST['form']) && $_POST['form'] == 'login') {
             
+            // Vérifie si les champs sont vides
             if($_POST['login'] == '' || $_POST['pwd'] == '') {
                 $msg = getMessage('Veuillez remplir tous les champs', 'error');               
             }else{
 
+                // On récupère les données du formulaire
                 $datas = $_POST;
 
+                // Appel de la fonction d'identification
+                    // Utiliser cette fonction si les mots de passe sont en clair dans la DB
                     $user = userIdentificationDB($conn, $datas);
 
+                    // Utiliser cette fonction si les mots de passe sont hashés dans la DB
+                    // $user = userIdentificationWithHashPwdDB($conn, $datas);            
+
+                // On vérifie si on a une adresse email dans le tableau $user, si c'est le cas on est connecté
                 (!empty($user['email']))? $connexionSuccessfull = true : $connexionSuccessfull = false;
                 
             }
         }
 
+        // Si on est connecté, on initialise les variables de session et on redirige vers la page de gestion
         if($connexionSuccessfull === true) {
             $_SESSION['IDENTIFY'] = true;
             $_SESSION['user_email'] = $user['email'];
             header('Location: manager.php');     
-
+        // Dans le cas contraire on affiche un message d'erreur, il y a eu une erreur d'identification
         }elseif($connexionSuccessfull === false){
             $msg = getMessage('Votre email et/ou votre mot de passe sont erronés', 'error');
             
@@ -48,7 +60,7 @@
 <body>
     <div class="container">
         <div id="header-logo">
-        <h1><a href="index.php"><?php echo APP_NAME; ?></a></h1>
+        <h1><a href="index.php"><?= APP_NAME; ?></a></h1>
         </div>
         <div id="main-menu">
             <?php displayNavigation(); ?>
